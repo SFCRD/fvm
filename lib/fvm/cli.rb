164f879efe4a48a00aba0ca54c37fd6155c5a4c3
Jeremy Ruppel <jeremy.ruppel@gmail.com>
2011-02-01 14:54:20 -0800

    refactor cli a bit

--- lib/fvm/cli.rb
+++ lib/fvm/cli.rb
@@ -28,15 +28,10 @@ module Fvm
         puts "Creating symlinks..."
         
         local = Fvm::Builds::Local.new( unzipped )
-        
+        # unlink all symlinks
         unlink
-        
-        local.bins.each do |bin|
-          
-          linker.link( bin.name, bin.path )
-          
-        end
-
+        # link to the selected build's bins
+        local.bins.each { |bin| linker.link( bin.name, bin.path ) }
       end
       
     end
@@ -66,76 +61,19 @@ module Fvm
       # unlink all symlinks
       unlink
       # link to the selected build's bins
-      
-      
-      # 
-      # REFACTOR
-      # vvvvvvvv
-      #
-      local.bins.each do |bin|
-
-        linker.link( bin.name, bin.path )
-        
-      end
+      local.bins.each { |bin| linker.link( bin.name, bin.path ) }
     end
-    # desc 'list', 'List available Flex SDK versions'
-    # method_option :local, :type => :boolean, :default => false, :aliases => "-l", :desc => "List existing Flex SDK installations"
-    # method_option :remote, :type => :boolean, :default => false, :aliases => "-r", :desc => "List remote Flex SDK downloads"
-    # def list
-    #   puts 'LIST!'
-    #   if options.local?
-    #     puts 'list local'
-    #   elsif options.remote?
-    #     puts 'list remote'
-    #     
-    #     parser = Fvm::Parsers::Flex4.new
-    #     
-    #     tables = parser.parse( "http://opensource.adobe.com/wiki/display/flexsdk/Download+Flex+4" )
-    #     
-    #     builds = [ ]
-    #     
-    #     tables.each do |table|
-    #       
-    #       puts table[ :name ].color( :black ).bright
-    #       
-    #       table[ :builds ].each do |build|
-    #         
-    #         builds << Fvm::Builds::Remote.new( build[ 'Build' ], build[ 'Build Date' ] )
-    #         
-    #         puts " * %s %s %s" % [ build[ 'Build' ], build[ 'Build Date' ], build[ 'Milestone' ] || '' ]
-    #         
-    #       end
-    #       
-    #     end
-    #     
-    #     
-    #     
-    #   else
-    #     puts 'list local [default]'
-    #   end
-    # end
-    # desc 'use', 'Symlink to a specific Flex SDK version'
-    # def use
-    #   puts 'TODO use'
-    #   
-    #   build = locals.choose!
-    #   
-    #   if build.active?
-    #     puts "Flex SDK build #{build.version} already active"
-    #   else
-    #     linker.unlink!
-    #     linker.link build
-    #   end
-    #   
-    # end
     
     protected
+    
     def remotes_url
       "http://opensource.adobe.com/wiki/display/flexsdk/Download+Flex+4"
     end
+    
     def remotes
       @remotes ||= Fvm::Lists::Remotes.new( remotes_url )
     end
+    
     def locals_path
       if true
         File.expand_path( File.join( File.dirname( __FILE__ ), '..', '..', 'test', 'fixtures/lists' ) )
@@ -143,19 +81,23 @@ module Fvm
         '~/Developer/SDKs/'
       end
     end
+    
     def locals
       @locals ||= Fvm::Lists::Locals.new( locals_path )
     end
+    
     def links_path
       if true
-        File.expand_path( File.join( File.dirname( __FILE__ ), '..', '..', 'test', 'fixtures/lists' ) )
+        File.expand_path( File.join( File.dirname( __FILE__ ), '..', '..', 'test', 'fixtures/linkers' ) )
       else
         '/opt/local/bin'
       end
     end
+    
     def linker
       @linker ||= Fvm::Linkers::Linker.new( locals_path )
     end
+    
     def installer
       @installer ||= Fvm::Installers::Wget.new
     end
