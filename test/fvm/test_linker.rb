7193cdd44efad7cd6acd0487282a2cb54f27671b
Jeremy Ruppel <jeremy.ruppel@gmail.com>
2011-02-23 10:36:21 -0800

    fix linker tests

--- test/fvm/test_linker.rb
+++ test/fvm/test_linker.rb
@@ -5,7 +5,8 @@ require "fvm/cli/linker"
 class TestFvmCliLinker < Test::Unit::TestCase
   # setup
   def setup
-    @linker = Fvm::CLI::Linker.new( 'test/fixtures/linker', [ 'executable' ] )
+    @dir = Pathname.new( File.dirname( __FILE__ ) ).join( '../fixtures/linker' )
+    @linker = Fvm::CLI::Linker.new( @dir.to_s, [ 'executable' ] )
   end
   # teardown
   def teardown
@@ -13,20 +14,20 @@ class TestFvmCliLinker < Test::Unit::TestCase
   end
   # finds correct list of files in the given directory
   def test_finds_correct_list_of_files_in_the_given_directory
-    assert_equal( [ 'test/fixtures/linker/bin/executable' ], @linker.files( 'test/fixtures/linker/bin' ) )
+    assert_equal( [ @dir.join( 'bin', 'executable' ).to_s ], @linker.files( @dir.join( 'bin' ) ) )
   end
   # creates symlinks for files found in the given directory
   def test_creates_symlinks_for_files_found_in_the_given_directory
-    assert_equal( false, File.exist?( 'test/fixtures/linker/executable' ) )
-    @linker.link( 'test/fixtures/linker/bin' )
-    assert_equal( true, File.exist?( 'test/fixtures/linker/executable' ) )
+    assert_equal( false, @dir.join( 'executable' ).exist? )
+    @linker.link( @dir.join( 'bin' ) )
+    assert_equal( true, @dir.join( 'executable' ).exist? )
   end
   # can unlink all symlinks it has created
   def test_can_unlink_all_symlinks_it_has_created
-    assert_equal( false, File.exist?( 'test/fixtures/linker/executable' ) )
-    @linker.link( 'test/fixtures/linker/bin' )
-    assert_equal( true, File.exist?( 'test/fixtures/linker/executable' ) )
+    assert_equal( false, @dir.join( 'executable' ).exist? )
+    @linker.link( @dir.join( 'bin' ) )
+    assert_equal( true, @dir.join( 'executable' ).exist? )
     @linker.unlink!
-    assert_equal( false, File.exist?( 'test/fixtures/linker/executable' ) )
+    assert_equal( false, @dir.join( 'executable' ).exist? )
   end
 end
