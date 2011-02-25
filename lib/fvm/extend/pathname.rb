89600ea1153b35f06e016b7c56977499bead96d3
Jeremy Ruppel <jeremy.ruppel@gmail.com>
2011-02-25 11:42:34 -0800

    make bins directory writable and group-owned

--- lib/fvm/extend/pathname.rb
+++ lib/fvm/extend/pathname.rb
@@ -1,7 +1,9 @@
 require 'pathname'
 
 class Pathname
+  
   def to_bash
-    expand_path.to_s.sub /^#{File.expand_path '~'}/, '$HOME'
+    expand_path.to_s.sub( /^#{File.expand_path( '~' )}/, '$HOME')
   end
+  
 end
